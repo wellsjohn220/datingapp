4c42f66df133416500389d87e51580678daef9d7
John Ye <windsorjohn@220@gmail.com>
2021-09-11 10:59:49 +1000

    End of section 2

---
+++ API/Start SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "John");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Jane");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Rose");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Jack");
